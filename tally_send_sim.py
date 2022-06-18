c0da7e44223d8abce8bb74de0f0c75bc7e752420
matt08-prog <matthewroloff@gmail.com>
2022-06-18 17:45:42 -0600

    final

--- tally_send_sim.py
+++ tally_send_sim.py
@@ -1,14 +1,34 @@
 import socket, time, random
+from time import sleep
+import serial
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+Green_pins = [11,12,13]
+Red_pins = [15,16,18]
+
+for(pin in Green_pins):
+    GPIO.setup(pin, GPIO.IN)
+for(pin in Red_pins):
+    GPIO.setup(pin, GPIO.IN)
 
 SERVER_ADDRESS_PORT = ("0.0.0.0", 40001)
 
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+ser = serial.Serial('/dev/serial0', 38400, timeout=1)
+ser.close()
+ser.open()
+sleep(1)
 
 def hex_print(byte_seq):
     for b in byte_seq:
         print(hex(b), end=' ')
     print('')
+    print("\n")
+    
 
 def get_message(cam, status):
     bmsg = [0x80 + cam]
@@ -20,8 +40,14 @@ def get_message(cam, status):
     text += '       '
     
     # status = 0x32(green) or 0x31(red). 0x30(neither)
-    status = 0x32 if status else 0x30
-    bmsg.append(status)
+    if(status == "Green"):
+        Status == 0x32
+    elif(status == "Red"):
+        Status = 0x31
+    else:
+        Status = 0x30
+#     Status = 0x32 if status else 0x30
+    bmsg.append(Status)
     # bmsg.extend([0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63])
     for t in text:
         bmsg.append(int.from_bytes(t.encode(), byteorder='big'))
@@ -29,30 +55,54 @@ def get_message(cam, status):
 
 def send_message(seq):
     # Send to server using created UDP socket
-    UDPClientSocket.sendto(seq, SERVER_ADDRESS_PORT)        
+    print("Hex Print:     ")
+    hex_print(seq)
+    UDPClientSocket.sendto(seq, SERVER_ADDRESS_PORT)
+    
+#     print("about to send")
+    ser.write(seq)
+#     ser.write(bytes(seq, 'utf-8'))
+#     print("sending")
+#     ser.close()
 
 last = 0
-for i in range(5):
-    msg = get_message(i+1, False)
+for i in range(3):
+    msg = get_message(i+1, "Off")
     send_message(bytearray(msg))
-
+#     sleep(4)
 while True:
-    now = random.randrange(5) + 1
-    while now == last:
-        now = random.randrange(5) + 1
-    print('Change:', now)
-    on = get_message(now, True)
-    off = get_message(last, False)
-    last = now
-    hex_print(on)
-    send_message(bytearray(on))
-    wipe = random.randrange(9)
-    if wipe == 7:
-        m = random.randrange(6) + 1
-        print('wipe:', m)
-        time.sleep(m)
-    hex_print(off)
-    send_message(bytearray(off))
-    
-    print()
-    time.sleep(2)
+
+#     now = random.randrange(5) + 1
+#     while now == last:
+#         now = random.randrange(5) + 1
+#     print('Change:', now)
+    for(pin in Green_pins):
+        if(GPIO.input(pin) == 1):
+            on = get_message(now,"Green")
+            sleep(0.1)
+            off = get_message(Green_last, "Off")
+            Green_last = pin
+        #     hex_print(on)
+            send_message(bytearray(on))
+            send_message(bytearray(off))
+            
+    for(pin in Red_pins):
+        if(GPIO.input(pin) == 1):
+            on = get_message(now, "Red")
+            sleep(0.1)
+            off = get_message(Green_last, "Off")
+            Green_last = pin
+        #     hex_print(on)
+            send_message(bytearray(on))
+            send_message(bytearray(off))
+    #     wipe = random.randrange(9)
+    #     time.sleep(2)
+    #     if wipe == 7:
+    #         m = random.randrange(6) + 1
+    #         print('wipe:', m)
+    #         time.sleep(m)
+    #     hex_print(off)
+        
+        
+    #     print()
+    #     time.sleep(3)
